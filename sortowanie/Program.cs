730383a8eb543b4bc5a23e8b0908b4b590c1d9b8
sebastianteb <167518767+sebastianteb@users.noreply.github.com>
2024-04-19 10:56:07 +0200

    Add files via upload

---
+++ sortowanie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        // Utworzenie obiektu klasy Sort
+         Sort sorter = new Sort();
+
+        // Pobranie danych od użytkownika
+        int[] tablica = new int[10]; 
+        for (int i = 0; i < tablica.Length; i++)
+        { 
+            Console.Write($"Podaj liczbę {i + 1}: "); 
+            tablica[i] = int.Parse(Console.ReadLine());
+        } 
+        // Posortowanie tablicy
+        sorter.Sortuj(tablica); 
+        
+        // Wyświetlenie posortowanej tablicy
+        Console.WriteLine("\nPosortowana tablica:");
+        for (int i = 0; i < tablica.Length; i++) 
+        {
+        Console.Write(tablica[i] + " "); 
+        } 
+    }
+}
---
+++ sortowanie/Sort.cs
@@ -0,0 +1,61 @@
+﻿class Sort 
+{ 
+    private int[] tablica;
+    /************************************** 
+     * nazwa funkcji:        Sortuj 
+     * parametry wejściowe:  int[] tablica
+     * wartość zwracana:     nic
+     * autor:                Sebastian Bolz
+     * *************************************/
+    public void Sortuj(int[] tablica)
+    {
+        this.tablica = tablica;
+        for (int i = 0; i < tablica.Length - 1; i++)
+        {
+            int indeksMaksymalnej = i;
+            for (int j = i + 1; j < tablica.Length; j++)
+            {
+                if (tablica[j] > tablica[indeksMaksymalnej])
+                {
+                    indeksMaksymalnej = j;
+                }
+            }
+            Zamien(i, indeksMaksymalnej);
+        }
+    }
+    /************************************** 
+* nazwa funkcji:        Zamien
+* parametry wejściowe:  indeks1 indeks2
+* wartość zwracana:     nic
+* autor:                Sebastian Bolz
+* *************************************/
+    private void Zamien(int indeks1, int indeks2)
+    {
+        int temp = tablica[indeks1];
+        tablica[indeks1] = tablica[indeks2];
+        tablica[indeks2] = temp;
+    
+    }
+    
+    /************************************** 
+* nazwa funkcji:        ZnajdzMaksymalny
+* parametry wejściowe:  tablica odIndeksu
+* wartość zwracana:     indeksMaksymalnej
+* autor:                Sebastian Bolz
+* *************************************/
+    private int ZnajdzMaksymalny(int[] tablica, int odIndeksu)
+    {
+     int indeksMaksymalnej = odIndeksu;
+        
+        for (int i = odIndeksu + 1; i < tablica.Length; i++)
+        {
+            
+            if (tablica[i] > tablica[indeksMaksymalnej])
+            { 
+                indeksMaksymalnej = i;
+            }
+        }
+      
+        return indeksMaksymalnej; 
+    } 
+}
